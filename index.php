1cf3f1c1f0f7535c6a6b347046ad327cac88d344
Rusaln-IT <ruslan.r.f@yandex.ru>
2026-04-06 13:09:21 +0500

    ╨а╨░╨╖╨┤╨╡╨╗╨╡╨╜╨╕╨╡ ╨╗╨╛╨│╨╕╨║╨╕ ╤А╨░╨▒╨╛╤В╤Л ╨┐╤А╨╕╨╗╨╛╨╢╨╡╨╜╨╕╤П  Controller тЖТ Service тЖТ Model

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+session_start();
+
+use App\Core\Router;
 
 
- echo 'Site works!';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$router = new Router();
+
+
+
+$router->get('/', 'HomeController@index');
+
+$router->get('/category', 'BlogController@redirectToHome');
+$router->get('/category/{slug}', 'BlogController@category');
+$router->get('/ajax/category-posts', 'HomeController@getAllPostsByCategory');
+
+$router->get('/post/{slug}', 'BlogController@show');
+
+
+$router->dispatch($_SERVER['REQUEST_URI']);
