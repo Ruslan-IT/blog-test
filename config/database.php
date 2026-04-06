cf3149be2de6c5bc343870b2ef0cf77cb2e8ddc3
Rusaln-IT <ruslan.r.f@yandex.ru>
2026-04-06 13:16:05 +0500

    ╨а╨░╨╖╨┤╨╡╨╗╨╡╨╜╨╕╨╡ ╨╗╨╛╨│╨╕╨║╨╕ ╤А╨░╨▒╨╛╤В╤Л ╨┐╤А╨╕╨╗╨╛╨╢╨╡╨╜╨╕╤П  Controller тЖТ Service тЖТ Model

--- config/database.php
+++ config/database.php
@@ -1,8 +1,16 @@
 <?php
 
-return  [
+/*return  [
     'host' => 'MySQL-8.0',
     'user' => 'root',
     'password' => '',
     'dbname' => 'tz',
+];*/
+
+
+return  [
+    'host' => 'localhost',
+    'user' => 'ruslabq5_tz',
+    'password' => 'TwuI3TSaJ0O*',
+    'dbname' => 'ruslabq5_tz',
 ];
